205cfbeb5bc2f166b6e86333f0e964210556649e
Yom3n <kamilbudnik94@gmail.com>
2024-10-20 11:20:31 +0200

    Fixed json outputs to be a snake_case instead of PascalCase

--- db/database/models.go
+++ db/database/models.go
@@ -11,8 +11,8 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Name      string
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
 }
