5cbbc507328ec5940854f8c496296b111b58ce36
Yom3n <kamilbudnik94@gmail.com>
2024-10-20 10:31:34 +0200

    Handled craeting user with real name

--- services/users/users.go
+++ services/users/users.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 
@@ -22,12 +23,23 @@ func (h *Handler) RegisterRoutes(router *http.ServeMux) {
 }
 
 func (h *Handler) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	jsonDecoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	jsonDecoder.Decode(&params)
+	if params.Name == "" {
+		utils.RespondWithError(w, 400, "name is required")
+		return
+	}
+
 	user, err := h.db.CreateUser(r.Context(),
 		database.CreateUserParams{
 			ID:        uuid.New(),
 			CreatedAt: time.Now().UTC(),
 			UpdatedAt: time.Now().UTC(),
-			Name:      "Eric",
+			Name:      params.Name,
 		})
 	if err != nil {
 		utils.RespondWithError(w, 500, err.Error())
