36bf37e6ae0071c1de3a6a8833c2943ec1edb17b
Yom3n <kamilbudnik94@gmail.com>
2024-10-20 16:20:49 +0200

    Added apikey to users

--- db/schema/002_users_api_key.sql
+++ db/schema/002_users_api_key.sql
@@ -1,3 +1,8 @@
 -- +goose Up
-ALTER TABLE users ADD
-    apy_key Text
+ALTER TABLE users ADD COLUMN api_key VARCHAR(64) UNIQUE NOT NULL DEFAULT (
+    encode(sha256(random()::text::bytea), 'hex')
+);
+
+
+-- +goose Down
+ALTER TABLE users DROP COLUMN api_key;
